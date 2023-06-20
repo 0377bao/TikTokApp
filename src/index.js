c1f178d2da32bdbcdd2c335a828593a074dc14e9
Bao <hbao27121@gmail.com>
2023-06-20 18:06:19 +0700

    push code the first time

--- src/index.js
+++ src/index.js
@@ -4,15 +4,43 @@ import App from '~/App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from './components/GlobalStyle';
 import Grid from './components/Grid/Grid';
+import { ErrorIcon } from './components/Icons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
     <React.StrictMode>
-        <Grid>
-            <GlobalStyle>
-                <App />
-            </GlobalStyle>
-        </Grid>
+        {window.innerWidth >= 1024 ? (
+            <Grid>
+                <GlobalStyle>
+                    <App />
+                </GlobalStyle>
+            </Grid>
+        ) : (
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    width: '100%',
+                    height: '100vh',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <ErrorIcon width="20rem" height="20rem" />
+                <span
+                    style={{
+                        fontSize: '2rem',
+                        padding: '15px 30px',
+                        textAlign: 'center',
+                        fontWeight: '700',
+                        lineHeight: '2.8rem',
+                    }}
+                >
+                    Trang web hiện không hỗ trợ phiên bản trên mobile. Xin lỗi vì sự bấc tiện này
+                </span>
+            </div>
+        )}
     </React.StrictMode>,
 );
 
